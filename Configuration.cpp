9509f6a31f8d50a52ec6dacc3f61ac5ed6c85fa5
makenzielarsen <makenzie97larsen@gmail.com>
2017-04-14 10:42:55 -0600

    Implementing Configuration

--- Configuration.cpp
+++ Configuration.cpp
@@ -2,3 +2,19 @@
 // Created by Makenzie Larsen on 4/14/17.
 //
 
+#include "Configuration.h"
+
+string Configuration::getStringValue(string key) {
+    string value = map.find(key);
+    return value;
+}
+
+int Configuration::getIntValue(string key) {
+    string value = getStringValue(key);
+    return stoi(value);
+}
+
+double Configuration::getDoubleValue(string key) {
+    string value = getStringValue(key);
+    return stod(key);
+}
