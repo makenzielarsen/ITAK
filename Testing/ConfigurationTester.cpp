742dab8d0a67b43ebf8eea3a161791c445018856
makenzielarsen <makenzie97larsen@gmail.com>
2017-04-14 12:42:33 -0600

    Setting up Configuration Tester

---
+++ Testing/ConfigurationTester.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Makenzie Larsen on 4/14/17.
+//
+#include "ConfigurationTester.h"
+#include "../Configuration.h"
+
+void ConfigurationTester::testGetStringValue() {
+    Configuration configuration;
+}
+
+void ConfigurationTester::testGetIntValue() {
+
+}
+
+void ConfigurationTester::testGetDoubleValue() {
+
+}
---
+++ Testing/ConfigurationTester.h
@@ -0,0 +1,15 @@
+//
+// Created by Makenzie Larsen on 4/14/17.
+//
+
+#ifndef ITAK_CONFIGURATIONTESTER_H
+#define ITAK_CONFIGURATIONTESTER_H
+
+class ConfigurationTester {
+public:
+    void testGetStringValue();
+    void testGetIntValue();
+    void testGetDoubleValue();
+};
+
+#endif //ITAK_CONFIGURATIONTESTER_H
