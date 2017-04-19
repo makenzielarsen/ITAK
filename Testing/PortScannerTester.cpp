c87d7c00e213a996d4199dae0f1f4891be939258
makenzielarsen <makenzie97larsen@gmail.com>
2017-04-19 12:43:16 -0600

    Implementing port scanner test cases

--- Testing/PortScannerTester.cpp
+++ Testing/PortScannerTester.cpp
@@ -3,7 +3,9 @@
 //
 
 #include "PortScannerTester.h"
+#include "Test.h"
 #include "../Configuration.h"
+#include "../PortScanner.h"
 
 void PortScannerTester::testConstructor() {
     Configuration configuration;
@@ -12,7 +14,26 @@ void PortScannerTester::testConstructor() {
 }
 
 void PortScannerTester::testConfigurationValid() {
+    Configuration configuration;
+    configuration.set("Likely Port Attack Count", 40);
+    configuration.set("Possible Port Attack Count", 30);
 
+    PortScannerAnalyzer portScannerAnalyzer(configuration);
+    if (portScannerAnalyzer.checkConfigurationValid()) {
+        TEST("true", "true");
+    } else {
+        TEST ("false", "true");
+    }
+
+    Configuration configuration1;
+    configuration.set("Likely Port Attack Count", 40);
+
+    PortScannerAnalyzer portScannerAnalyzer1(configuration1);
+    if (!portScannerAnalyzer1.checkConfigurationValid()) {
+        TEST("true", "false");
+    } else {
+        TEST("false", "false");
+    }
 }
 
 void PortScannerTester::testRun() {
--- Testing/testingMain.cpp
+++ Testing/testingMain.cpp
@@ -7,22 +7,32 @@
 #include "DenialOfServiceTester.h"
 #include "PortScannerTester.h"
 
+using namespace std;
+
 int main(){
+    cout << "Testing Configuration" << endl;
     ConfigurationTester configurationTester;
     configurationTester.testGetStringValue();
     configurationTester.testGetIntValue();
     configurationTester.testGetDoubleValue();
+    cout << endl;
 
+    cout << "Testing Result Set" << endl;
     ResultSetTester resultSetTester;
     resultSetTester.testGetResults();
+    cout << endl;
 
+    cout << "Testing Denial Of Service Analyzer" << endl;
     DenialOfServiceTester denialOfServiceTester;
     denialOfServiceTester.testConfigurationValid();
     denialOfServiceTester.testConstructor();
     denialOfServiceTester.testRun();
+    cout << endl;
 
+    cout << "Testing Port Scanner Analyzer" << endl;
     PortScannerTester portScannerTester;
     portScannerTester.testConfigurationValid();
     portScannerTester.testConstructor();
     portScannerTester.testRun();
+    cout << endl;
 }
